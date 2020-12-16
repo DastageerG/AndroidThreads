7d39b82124b0555177861b6d49dd7181d8bafee1
Ghulam Dastageer <dastageerg44@gmail.com>
2020-12-16 01:32:54 -0800

    Handler/Runnable threads

---
+++ app/src/main/java/com/example/androidthreads/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.androidthreads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private TextView textView;
+    private ScrollView scrollView;
+    private ProgressBar progressBar;
+    Thread thread;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        scrollView = findViewById(R.id.scrollView);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        textView.setText(R.string.lorem);
+
+    }
+
+    public void runCode(View view)
+    {
+        log("Code Running");
+        progressBar.setVisibility(View.VISIBLE);
+        Runnable runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+               progressBar.setVisibility(View.GONE);
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable,3000);
+
+
+
+    } // runCode closed
+    private void log(String message)
+    {
+        textView.append(message+"\n");
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
+    public void clearText(View view)
+    {
+        textView.setText("");
+        scrollView.fullScroll(View.FOCUS_UP);
+    }
+
+} // mainActivity closed
